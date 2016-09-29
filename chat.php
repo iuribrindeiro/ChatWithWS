0e986b9ce8b95128668533a2b5b3ab606f1586c3
iuri <iuribrindeiro@gmail.com>
2016-09-29 01:23:38 -0300

    first commit

--- chat.php
+++ chat.php
@@ -1,7 +1,7 @@
 <?php
 require(dirname(__FILE__).'/vendor/autoload.php');
 
-$pusher = new Pusher('235524ad737c76994c6b', '8b73ab58ac05744b399a', '253761');
+$pusher = new Pusher('API_KEY', 'APP_SECRET', 'APP_ID');
 
 $mensagem = $_POST['mensagem'];
 
